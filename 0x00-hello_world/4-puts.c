45c151ac4ed1bb9f8b78a5f451ca28018a3dee13
Mackibe <ndichujames2030@gmail.com>
2022-06-03 00:50:11 -0700

    tasks 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * prints Programming is like building a multilingual puzzle.
+ * returns 0
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
