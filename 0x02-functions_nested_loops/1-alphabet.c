7db4ce5c1024bbfb12c5cd6d55f961a6c2a7742e
Ndichu James <34843526+MacKibe@users.noreply.github.com>
2022-06-07 20:07:39 +0300

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print alphabet- prints alphabet in lowercase
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		printf("%c ", ch);
+	}
+	printf('\n');
+}
