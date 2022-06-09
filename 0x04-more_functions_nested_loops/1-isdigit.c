75b157477216341b8d27db10f88efd7e1b92ea75
Ndichu James <34843526+MacKibe@users.noreply.github.com>
2022-06-09 22:19:47 +0300

    1-isdigit.c added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - writes the character c to stdout
+ * @c: the character being checked
+ * Return: 1 if digit else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= 9)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
