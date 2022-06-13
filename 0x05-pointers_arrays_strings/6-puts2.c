41ef0c92bf4476208df924e3b18a08bddc9544cc
Ndichu James <34843526+MacKibe@users.noreply.github.com>
2022-06-13 23:35:34 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character, followed by a new line.
- * @str: An input string
- * Return: Nothing
- */https://github.com/MacKibe/alx-low_level_programming/pulse
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: string pointer
+ * Return: void
+ */
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+	int i = 0;
 
-	while (str[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
-
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
